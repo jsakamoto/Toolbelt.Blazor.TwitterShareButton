39ef736beb3dd1748b60d537795b22cb3aace644
jsakamoto <junichi.jp@gmail.com>
2018-12-11 23:43:09 +0900

    Initial commit.

---
+++ README.md
@@ -0,0 +1,43 @@
+# Blazor Tweet Button [![NuGet Package](https://img.shields.io/nuget/v/Toolbelt.Blazor.TwitterShareButton.svg)](https://www.nuget.org/packages/Toolbelt.Blazor.TwitterShareButton/)
+
+## Summary
+
+A Tweet Button component  for Blazor.
+
+## How to install and use?
+
+### 1. Installation and Registration
+
+**Step.1-1** Install the library via NuGet package, like this.
+
+```shell
+> dotnet add package Toolbelt.Blazor.TwitterShareButton
+```
+
+**Step.1-2** Register tag helper to your Blazor app, like this.
+
+```csharp
+// _ViewImports.cshtml
+...
+@addTagHelper *, Toolbelt.Blazor.TwitterShareButton
+```
+
+### 2. Usage in your Blazor component (.cshtml)
+
+You can use `TwitterShareButton` component.
+
+If you implement your component like this,
+
+```html
+<TwitterShareButton 
+  Text="@($"Current Count is {currentCount}")"
+  Size="Large"></TwitterShareButton>
+```
+
+Then you will get this.
+
+![fig.1](https://github.com/jsakamoto/Toolbelt.Blazor.TwitterShareButton/blob/master/.assets/fig1.png)
+
+## License
+
+[Mozilla Public License Version 2.0](https://github.com/jsakamoto/Toolbelt.Blazor.TwitterShareButton/blob/master/LICENSE)
---
+++ Toolbelt.Blazor/content/script.ts
@@ -0,0 +1,40 @@
+﻿declare var twttr: any;
+
+//var twttr = {
+//    widgets: {
+//        createShareButton: function (shareUrl: string, targetElement: HTMLElement, ...args: any[]) {
+//            console.log('CALLED CREATESHAREBUTTON', shareUrl, targetElement);
+//            targetElement.innerText = `[shareUrl: "${shareUrl}"]`;
+//        }
+//    }
+//};
+
+namespace Toolbelt.Blazor.TwitterShareButton {
+
+    (function () {
+        const d = document;
+        const s = 'script';
+        const id = 'twitter-wjs';
+        const fjs = d.getElementsByTagName(s)[0];
+        if (!d.getElementById(id)) {
+            const js = d.createElement(s);
+            js.id = id;
+            js.src = '//platform.twitter.com/widgets.js';
+            fjs.parentNode.insertBefore(js, fjs);
+        }
+    })();
+
+    export function create(targetElement: HTMLElement, options: any): void {
+        targetElement.innerHTML = "";
+        if (typeof (twttr) !== 'undefined') {
+            createCore(targetElement, options);
+        }
+        else {
+            setTimeout(() => create(targetElement, options), 1000);
+        }
+    }
+
+    function createCore(targetElement: HTMLElement, options: any): void {
+        twttr.widgets.createShareButton(options.url, targetElement, options);
+    }
+}
