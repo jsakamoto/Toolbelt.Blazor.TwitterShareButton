06395f750a7f4e30d3db7a7d5b8a3c1951584e5a
jsakamoto <junichi.jp@gmail.com>
2020-02-01 12:24:28 +0900

    Update sample site to Blazor v.3.2 preview 1.

---
+++ SampleSites/Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using SampleSite.Client.Services;
+using SampleSite.Components;
+using SampleSite.Components.Services;
+
+namespace SampleSite.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+            builder.Services.AddSingleton<IWeatherForecastService, WeatherForecastService>();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
